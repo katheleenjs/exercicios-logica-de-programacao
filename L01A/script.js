94b20f8811d7353f39aa368863485b211e63ffcb
katheleen <katheleenjds@gmail.com>
2026-03-08 19:42:25 -0300

    Conversão Celsius para Fahrenheit

---
+++ L01A/script.js
@@ -0,0 +1,8 @@
+//  Exercício L01A - Conversão de Celsius para Fahrenheit
+let celsius = parseFloat(prompt("Digite a temperatura em Celsius:"));
+
+let fahrenheit = (9 * celsius + 160) / 5;
+
+alert("A temperatura em Fahrenheit é: " + fahrenheit.toFixed(1));
+
+// console.log("Temperatura em Fahrenheit:", fahrenheit);
